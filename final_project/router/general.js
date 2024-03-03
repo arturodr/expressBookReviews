f09519d7182f8d03da072d961a096be79423f66d
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-02 21:22:13 -0500

    Complete the code for getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,8 +52,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn] && Object.keys(books[isbn].reviews).length > 0) {
+    return res.status(200).send(JSON.stringify(books[isbn].reviews, null, 2));
+  } else {
+    return res.status(204).end();
+  }
 });
 
 module.exports.general = public_users;
