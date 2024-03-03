88738933b22384520008f9323ea504fce6eb7876
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-02 21:14:42 -0500

    Complete the code for getting the book details based on the title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,8 +40,14 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksWithTitle = Object.values(books).filter(book => book.title === title);
+
+  if (booksWithTitle.length > 0) {
+    return res.status(200).send(JSON.stringify(booksWithTitle, null, 2));
+  } else {
+    return res.status(204).end();
+  }
 });
 
 //  Get book review
