98f7ef361d69620792e607d3d72be6235ef5b16d
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-02 21:07:45 -0500

    Complete the code for getting the book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,13 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  // Assuming the index is the ISBN
+  if (books[isbn]) {
+    return res.status(200).send(JSON.stringify(books[isbn], null, 2));
+  } else {
+    return res.status(204).end();
+  }
  });
   
 // Get book details based on author
