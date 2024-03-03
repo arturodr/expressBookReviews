4206edafa8f28909159291e4e992369d0393fe4e
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-02 21:11:17 -0500

    Complete the code for getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksOfAuthor = Object.values(books).filter(book => book.author === author);
+
+  if (booksOfAuthor.length > 0) {
+    return res.status(200).send(JSON.stringify(booksOfAuthor, null, 2));
+  } else {
+    return res.status(204).end();
+  }
 });
 
 // Get all books based on title
